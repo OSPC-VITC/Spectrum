6e5fb1f79561f9bc8811a6e762706651b074c810
Jeswanth S <jeswanthselva12@gmail.com>
2025-03-28 18:38:24 +0530

    Updated sponsors

--- src/components/sections/TimelineSection.tsx
+++ src/components/sections/TimelineSection.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const TimelineSection: React.FC = () => {
   return (
     <div>
-      <h2 className="text-3xl font-bold mb-4">Timeline</h2>
+      <h2 className="text-3xl font-bold mb-4"></h2>
       {/* Add any introductory text or description here if needed */}
     </div>
   );
